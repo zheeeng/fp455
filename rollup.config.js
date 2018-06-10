d77dfda5449914cecd4455e7ae3ac3fc4e14f11f
Zheeeng <hi@zheeeng.com>
2018-06-10 12:10:39 +0800

    Update building script

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import bucklescript from 'rollup-plugin-bucklescript'
+
+const buildBin = process.env.NODE_ENV === 'bin'
+
+const coreBuildConfig = {
+  input: 'src/Fp455.re',
+  output: {
+    file: 'dist/core.js',
+    format: 'cjs',
+  },
+  plugins: [bucklescript({
+  })],
+}
+
+const binBuildConfig = {
+  input: 'src/Main.re',
+  output: {
+    file: 'dist/bin.js',
+    format: 'cjs',
+  },
+  external: ['process'],
+  plugins: [bucklescript({
+    'package-specs': 'commonjs',
+  })],
+}
+
+export default buildBin ? binBuildConfig : coreBuildConfig
